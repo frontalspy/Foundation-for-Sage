7d410284c9e1e4f49631194da31885326f3a473b
frontalspy <frontalspy@users.noreply.github.com>
2017-02-07 12:40:52 +1100

    Whitespacing

--- templates/content.php
+++ templates/content.php
@@ -1,8 +1,8 @@
-<?php if (!defined('ABSPATH') ) { 
-    exit;  // Exit if accessed directly
+<?php if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
 } ?>
 
- <article <?php post_class(); ?>>
+<article <?php post_class(); ?>>
   <header>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php get_template_part('templates/entry-meta'); ?>
