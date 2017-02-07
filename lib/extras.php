e9e5b329a53ef883f2e1dbc7ce5ad0e1c35f8130
frontalspy <frontalspy@users.noreply.github.com>
2017-02-07 14:09:03 +1100

    Camels like nav menus

--- lib/extras.php
+++ lib/extras.php
@@ -36,7 +36,7 @@ function excerpt_more() {
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
 
-class foundation_Nav_Menu extends \Walker_Nav_Menu {
+class foundationNavMenu extends \Walker_Nav_Menu {
   public function start_lvl(&$output, $depth = 0, $args = array()) {
     $indent = str_repeat("\t", $depth);
     $output .= "\n$indent<ul class=\"menu\">\n";
