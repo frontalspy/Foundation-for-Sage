6b7850992f01464ae27cdcf70723f3dc307c7381
frontalspy <frontalspy@users.noreply.github.com>
2017-02-07 12:41:44 +1100

    Whitespacing

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,5 +1,5 @@
-<?php if (!defined('ABSPATH') ) { 
-    exit;  // Exit if accessed directly
+<?php if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
 } ?>
 
 <?php use Roots\Sage\Titles; ?>
