d81706ef69a81b3a9237db0c172981c8736675d5
frontalspy <frontalspy@users.noreply.github.com>
2017-02-07 14:17:29 +1100

    camel sniff

--- lib/extras.php
+++ lib/extras.php
@@ -36,7 +36,7 @@ function excerpt_more() {
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
 
-class foundationnavmenu extends \Walker_Nav_Menu {
+class foundationnavmenu extends \walkernavmenu {
   public function startlvl(&$output, $depth = 0, $args = array()) {
     $indent = str_repeat("\t", $depth);
     $output .= "\n$indent<ul class=\"menu\">\n";
