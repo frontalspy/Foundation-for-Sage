8b4a7abe466a0ca53802c69c0c8136cb08179fda
frontalspy <frontalspy@users.noreply.github.com>
2017-02-07 12:15:23 +1100

    Indentation

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,4 +1,6 @@
-<?php if( !defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
+<?php if (!defined('ABSPATH') ) { 
+    exit;  // Exit if accessed directly
+} ?>
 
 <?php the_content(); ?>
 <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
