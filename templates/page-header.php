a9ddccbde27d3735cae7f77da6aa0b49d354f285
frontalspy <frontalspy@users.noreply.github.com>
2017-02-07 12:16:34 +1100

    Indentation

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,4 +1,6 @@
-<?php if( !defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
+<?php if (!defined('ABSPATH') ) { 
+    exit;  // Exit if accessed directly
+} ?>
 
 <?php use Roots\Sage\Titles; ?>
 
