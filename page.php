980a33f2f10dc2887986c62dfe74ee75175a2f56
frontalspy <frontalspy@users.noreply.github.com>
2017-02-07 13:07:29 +1100

    Whitespaces

--- page.php
+++ page.php
@@ -2,7 +2,7 @@
     exit; // Exit if accessed directly
 } ?>
 
- <?php while (have_posts()) : the_post(); ?>
+<?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/page', 'header'); ?>
   <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
