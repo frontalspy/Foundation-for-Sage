b50baa5f47deb798eba1a1c2d2bdc71472063ab6
frontalspy <frontalspy@users.noreply.github.com>
2017-02-07 12:41:12 +1100

    Whitespacing

--- templates/footer.php
+++ templates/footer.php
@@ -1,8 +1,8 @@
-<?php if (!defined('ABSPATH') ) { 
-    exit;  // Exit if accessed directly
+<?php if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
 } ?>
 
- <footer class="content-info">
+<footer class="content-info">
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
