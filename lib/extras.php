e1d71e1f4178e73b7bfac4230b05c5941cc7d1a7
frontalspy <frontalspy@users.noreply.github.com>
2017-02-07 13:20:03 +1100

    Code standardisation

--- lib/extras.php
+++ lib/extras.php
@@ -37,8 +37,8 @@ function excerpt_more() {
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
 
 class Foundation_Nav_Menu extends \Walker_Nav_Menu {
-    function start_lvl(&$output, $depth = 0, $args = array()) {
-        $indent = str_repeat("\t", $depth);
-        $output .= "\n$indent<ul class=\"menu\">\n";
+  public function start_lvl(&$output, $depth = 0, $args = array()) {
+    $indent = str_repeat("\t", $depth);
+    $output .= "\n$indent<ul class=\"menu\">\n";
   }
 }
