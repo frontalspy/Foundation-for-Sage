d55dd22766b64a8f433a767d902bfc65e18a6a5e
frontalspy <frontalspy@users.noreply.github.com>
2017-02-07 12:15:05 +1100

    Indentation

--- templates/comments.php
+++ templates/comments.php
@@ -1,4 +1,6 @@
-<?php if( !defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
+<?php if (!defined('ABSPATH') ) { 
+    exit;  // Exit if accessed directly
+} ?>
 
 <?php
 if (post_password_required()) {
