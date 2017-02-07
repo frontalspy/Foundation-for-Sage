bdeb96924b6ff6d438d88b0fb53381959fa4a7f3
frontalspy <frontalspy@users.noreply.github.com>
2017-02-07 12:10:14 +1100

    Indentation

--- single.php
+++ single.php
@@ -1,3 +1,5 @@
-<?php if( !defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
+<?php if (!defined('ABSPATH') ) { 
+    exit;  // Exit if accessed directly
+} ?>
 
 <?php get_template_part('templates/content-single', get_post_type()); ?>
