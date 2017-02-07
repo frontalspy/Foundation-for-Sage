a2741fcbfd61d9fc64c5053ab8a31942f38f6637
frontalspy <frontalspy@users.noreply.github.com>
2017-02-07 12:15:48 +1100

    Indentation

--- templates/content.php
+++ templates/content.php
@@ -1,4 +1,6 @@
-<?php if( !defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
+<?php if (!defined('ABSPATH') ) { 
+    exit;  // Exit if accessed directly
+} ?>
 
  <article <?php post_class(); ?>>
   <header>
