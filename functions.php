d1c0a9e9a934671cd5395d43e54d073c1bf14bde
frontalspy <frontalspy@users.noreply.github.com>
2017-02-15 10:49:48 +1100

    Removed custom functions

--- functions.php
+++ functions.php
@@ -30,32 +30,3 @@ foreach ($sage_includes as $file) {
 
   require_once $filepath;
 }
-
-
-// Add async to jquery and defer all other scripts to prevent
-// render blocking.
-function nonRenderBlockScript($tag, $handle) {
-  if ('jquery' == $handle) {
-    return $tag;
-  } else {
-    return str_replace( ' src', ' defer src', $tag );
-  }
-}
-
-add_filter('script_loader_tag', 'nonRenderBlockScript', 10, 2);
-
-/* 
-
-Recommeded if using Advanced Custom Fields
-
-//add a options page in the admin panel
-if (function_exists('acf_add_options_page') ) {
-	acf_add_options_page((array(
-      'page_title' => 'Website Options',
-      'menu_title' => 'Website Options',
-      'menu_slug' => 'website_options',
-      'parent_slug' => '',
-      'position' => 'false',
-      'redirect' => 'false',    
-    )));
-} */
