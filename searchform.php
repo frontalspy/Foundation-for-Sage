ca7db70b767ceba52e5ea94ee570a8042bf5d291
frontalspy <frontalspy@users.noreply.github.com>
2017-05-29 09:53:18 +1000

    Updated formatting

--- searchform.php
+++ searchform.php
@@ -2,13 +2,9 @@
     exit; // Exit if accessed directly
 } ?>
 
- <form role="search" method="get" class="site-search js-search-submit" action="<?= esc_url(home_url('/')); ?>">
-  <div class="row">
-    <div class="small-12">
-      <div class="row collapse">
-        <input type="text" class="search-bar small-10" value="<?php echo get_search_query(); ?>" name="s" placeholder="<?php _e('Search', 'sage'); ?> <?php bloginfo('name'); ?>">
-        <input type="submit" class="button fa shrink" value="&#xf002;">
-      </div>
-    </div>
+<form role="search" method="get" class="site-search js-search-submit" action="<?php echo esc_url(home_url('/')); ?>">
+  <div class="small-12 row no-margin collapse">
+    <input type="text" class="search-bar small-10" value="<?php echo get_search_query(); ?>" name="s" placeholder="<?php _e('Search', 'sage'); ?> <?php bloginfo('name'); ?>">
+    <input type="submit" class="button fa small-12" value="&#xf002;">
   </div>
 </form>
