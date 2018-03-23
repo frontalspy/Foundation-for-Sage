033b694fa1ca87f6787f9feb4fdbe6fb84ccde39
frontalspy <frontalspy@users.noreply.github.com>
2018-03-23 16:02:21 +1100

    Removed Custom Nav Walker call
    The custom nav overwrites the clean module from soil, rendering it useless and more messy

--- templates/header.php
+++ templates/header.php
@@ -19,7 +19,7 @@
      <nav class="top-bar-section">
       <ul class="dropdown menu" data-dropdown-menu id="main-menu">
         <?php if (has_nav_menu('primary_navigation')) :?>
-          <?php wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'container' => '', 'items_wrap' => '%3$s', 'walker' => new Roots\Sage\Extras\foundationNavMenu()]);?>
+          <?php wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'container' => '', 'items_wrap' => '%3$s']);?>
         <?php endif;?>
       </ul>
       </nav>
